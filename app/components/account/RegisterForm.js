8c476f6d04a4de55535b23c4f5ce350dd7a5e4d2
lagoria21 <alberto.lagoria@gmail.com>
2020-09-22 21:13:23 -0300

    formulario validacion

--- app/components/account/RegisterForm.js
+++ app/components/account/RegisterForm.js
@@ -1,49 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Input, Icon, Button } from "react-native-elements";
 
-export default function RegisterForm(){
-    return(
-        <View style={styles.formContainer}>
-            <Input placeholder="Correo electronico" containerStyle={styles.inputForm}/>
-            <Input placeholder="Contraseña" 
-                   containerStyle={styles.inputForm} 
-                   password={true} 
-                   secureTextEntry={true}/>
-            <Input placeholder="Repetir Contraseña" 
-                    containerStyle={styles.inputForm}
-                    password={true} 
-                    secureTextEntry={true}
-                    />
-             <Button title="Unirse" containerStyle={styles.btnContainerRegister}
-             buttonStyle={styles.btnRegister}
-             />
-                     
+export default function RegisterForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+  const [formData, setFormData] = useState(defaultFormValue());
 
-        </View>
-    );
+  const onSubmit = () => {
+    console.log(formData);
+  };
+
+  const onChange = (e, type) => {
+    //console.log(type);
+    console.log(e.nativeEvent.text);
+  };
+
+  return (
+    <View style={styles.formContainer}>
+      <Input
+        placeholder="Correo electronico"
+        containerStyle={styles.inputForm}
+        onChange={(e) => onChange(e, "email")}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name="at"
+            iconStyle={styles.iconRight}
+          />
+        }
+      />
+      <Input
+        placeholder="Contraseña"
+        containerStyle={styles.inputForm}
+        password={true}
+        secureTextEntry={showPassword ? false : true}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.iconRight}
+            onPress={() => setShowPassword(!showPassword)}
+          />
+        }
+      />
+      <Input
+        placeholder="Repetir Contraseña"
+        containerStyle={styles.inputForm}
+        password={true}
+        secureTextEntry={showRepeatPassword ? false : true}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showRepeatPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.iconRight}
+            onPress={() => setShowRepeatPassword(!showRepeatPassword)}
+          />
+        }
+      />
+      <Button
+        title="Unirse"
+        containerStyle={styles.btnContainerRegister}
+        buttonStyle={styles.btnRegister}
+        onPress={onSubmit()}
+      />
+    </View>
+  );
 }
 
+function defaultFormValue() {
+  return {
+    email: "",
+    password: "",
+    repeatPassword: "",
+  };
+}
 
 const styles = StyleSheet.create({
-    formContainer: {
-     // flex: 1,
-     // alignItems: "center",
-     // justifyContent: "center",
-      marginTop: 30,
-    },
-    inputForm: {
-      width: "100%",
-      marginTop: 20,
-    },
-    btnContainerRegister: {
-      marginTop: 20,
-      width: "95%",
-    },
-    btnRegister: {
-      backgroundColor: "#00a680",
-    },
-    iconRight: {
-      color: "#c1c1c1",
-    },
-  });
+  formContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  inputForm: {
+    width: "100%",
+    marginTop: 20,
+  },
+  btnContainerRegister: {
+    marginTop: 20,
+    width: "95%",
+  },
+  btnRegister: {
+    backgroundColor: "#00a680",
+  },
+  iconRight: {
+    color: "#c1c1c1",
+  },
+});
